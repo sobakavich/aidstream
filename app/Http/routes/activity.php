7f4e9c8ba69b41ce0175e2845570371efa9af1be
Neha Rajbanshi <neha.rajbanshi@yipl.com.np>
2015-11-16 14:10:51 +0545

    AS-53 : Activity Element - Legacy Data - [x] Form view - [x] Save data - [x] Populate data - [x] Unit tests
    Conflicts:
    	app/Core/V201/IatiActivity.php
    	app/Http/routes/activity.php

--- app/Http/routes/activity.php
+++ app/Http/routes/activity.php
@@ -33,5 +33,6 @@ $router->group(
         $router->resource('activity.transaction', 'TransactionController');
         $router->resource('activity.transaction-upload', 'TransactionUploadController');
         $router->post('activity/{id}/update-status', 'ActivityController@updateStatus');
+        $router->resource('activity.legacy-data', 'LegacyDataController');
     }
 );
