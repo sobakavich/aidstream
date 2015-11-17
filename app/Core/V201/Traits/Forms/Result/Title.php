435b2f8e0dc8c26171c7787bb83b138d62574345
Lunek Suwal <lunek.suwal@yipl.com.np>
2015-11-17 16:46:22 +0545

    Activity Element - Result - [x] Form view - [x] Save data - [x] Populate data - [x] List Results - [x] View Result - [x] Unit tests

---
+++ app/Core/V201/Traits/Forms/Result/Title.php
@@ -0,0 +1,19 @@
+<?php namespace App\Core\V201\Traits\Forms\Result;
+
+/**
+ * Class Title
+ * @package App\Core\V201\Forms\Activity
+ */
+trait Title
+{
+
+    /**
+     * Return title form
+     * @return mixed
+     */
+    public function addTitles()
+    {
+        return $this->addCollection('title', 'Activity\Title');
+    }
+
+}
