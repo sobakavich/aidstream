da7539b41381f6ccbc09a1208e0f5e279016a8c2
Sachit Bir Singh Dongol <sachit.singh@yipl.com.np>
2016-02-24 11:17:45 +0545

    again testing..

--- app/Migration/Migrator/Data/OrganizationPublishedQuery.php
+++ app/Migration/Migrator/Data/OrganizationPublishedQuery.php
@@ -1,6 +1,7 @@
 <?php namespace App\Migration\Migrator\Data;
 
 use App\Migration\ActivityData;
+use Carbon\Carbon;
 
 class OrganizationPublishedQuery extends Query
 {
@@ -29,15 +30,17 @@ class OrganizationPublishedQuery extends Query
 
         //fetch published activity
         $organisationPublishedData = $this->connection->table('organisation_published')
-                                                  ->select('*')
-                                                  ->where('publishing_org_id', '=', $organizationId)
-                                                  ->get();
+                                                      ->select('*')
+                                                      ->where('publishing_org_id', '=', $organizationId)
+                                                      ->get();
 
         foreach ($organisationPublishedData as $data) {
             $organisationPublished[$data->filename] = [
                 'filename'              => $data->filename,
                 'published_to_register' => $data->pushed_to_registry,
                 'organization_id'       => $accountId,
+                'created_at'            => Carbon::now(),
+                'updated_at'            => Carbon::now()
             ];
         }
 
