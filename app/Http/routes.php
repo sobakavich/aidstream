d7d9501ca705bff751e66d0d9d9975905779387f
Kiran Shrestha <krnshrestha61@gmail.com>
2016-05-09 16:09:59 +0545

    AS-379 Activity view for Organization Admin
    - [x] Create view for user logs
    - [x] Display logs according to filter
    - [x] Create middleware organizationAdmin

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -220,3 +220,19 @@ $router->get(
         'uses'       => 'Complete\AdminController@updateOrganizationIdForUserActivities'
     ]
 );
+
+$router->get(
+    'user-logs',
+    [
+        'as'   => 'user-logs',
+        'uses' => 'Complete\UserLogController@search'
+    ]
+);
+
+$router->post(
+    'user-logs',
+    [
+        'as'   => 'user-logs.filter',
+        'uses' => 'Complete\UserLogController@search'
+    ]
+);
--- app/Services/Organization/OrganizationManager.php
+++ app/Services/Organization/OrganizationManager.php
@@ -200,4 +200,5 @@ class OrganizationManager
 
         return $userIds;
     }
+
 }
