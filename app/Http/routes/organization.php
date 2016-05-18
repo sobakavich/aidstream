ed68b29be3ca1e5e6bbd01e212251168effc272b
neha.rajbanshi <neha.rajbanshi@yipl.com.np>
2016-05-18 17:27:28 +0545

    AS-399 : Organization Xml View and download

--- app/Http/routes/organization.php
+++ app/Http/routes/organization.php
@@ -30,5 +30,21 @@ $router->group(
                 'uses' => 'OrganizationController@orgBulkPublishToRegistry'
             ]
         );
+
+        $router->get(
+            'organization/{orgId}/view/xml',
+            [
+                'as'   => 'view.organizationXml',
+                'uses' => 'OrganizationController@viewOrganizationXml'
+            ]
+        );
+
+        $router->get(
+            'organization/{orgId}/download/xml',
+            [
+                'as'   => 'download.organizationXml',
+                'uses' => 'OrganizationController@downloadOrganizationXml'
+            ]
+        );
     }
 );
---
+++ resources/views/Organization/xmlView.blade.php
@@ -0,0 +1,33 @@
+@extends('app')
+
+@section('title', 'Organization Data')
+
+@section('content')
+    <div class="container main-container">
+        <div class="row">
+            @include('includes.side_bar_menu')
+            <div class="col-xs-9 col-md-9 col-lg-9 content-wrapper">
+                <div class="element-panel-heading">
+                    <div class="element-panel-heading">
+                        <div><span class="pull-left">Organization</span></div>
+                        <span><a href="{{route('download.organizationXml', ['orgId' => $orgId])}}" class="btn btn-primary">Download Xml</a></span>
+                        <span><a href="{{ route('organization.show', $orgId) }}" class="btn btn-primary">View Organization</a></span>
+                    </div>
+                </div>
+                <ul>
+                    @forelse($messages as $message)
+                        <li>{!! $message !!}</li>
+                    @empty
+                        <li>Validated!!</li>
+                    @endforelse
+                </ul>
+                <br/>
+                @foreach($xmlLines as $key => $line)
+                    {{--*/ $number = $key + 1; /*--}}
+                    {{--*/ $class = array_key_exists($number, $messages) ? 'class="error"' : '' /*--}}
+                    <div id="{{ $number }}" {{$class}}><strong style="{{ array_key_exists($number, $messages) ? 'color:red': ''  }}">{{ $number }}</strong>{{ $line }}</div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+@endsection
