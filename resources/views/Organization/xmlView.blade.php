5cfcabb48e4dff32ea399cac907ff24b6687e839
Sweta Shrestha <sweta.shrestha@yipl.com.np>
2016-05-20 17:32:45 +0545

    theming of xml section

--- resources/views/Organization/xmlView.blade.php
+++ resources/views/Organization/xmlView.blade.php
@@ -8,24 +8,32 @@
             @include('includes.side_bar_menu')
             <div class="col-xs-9 col-md-9 col-lg-9 content-wrapper">
                 <div class="element-panel-heading">
-                    <div class="element-panel-heading">
-                        <div><span class="pull-left">Organization</span></div>
-                        <span><a href="{{route('download.organizationXml', ['orgId' => $orgId])}}" class="btn btn-primary">Download Xml</a></span>
-                        <span><a href="{{ route('organization.show', $orgId) }}" class="btn btn-primary">View Organization</a></span>
+                    <div>
+                        <div class="org-title"><span class="pull-left">Organization</span></div>
+                        <div class="panel-action-btn panel-xml-btn">
+                    <span><a href="{{ route('organization.show', $orgId) }}" class="back-to-organization">Back to
+                            Organization</a></span>
+                    <span><a href="{{route('download.organizationXml', ['orgId' => $orgId])}}" class="btn btn-primary">Download
+                            Xml</a></span>
+                        </div>
                     </div>
                 </div>
-                <ul>
-                    @forelse($messages as $message)
-                        <li>{!! $message !!}</li>
-                    @empty
-                        <li>Validated!!</li>
-                    @endforelse
-                </ul>
-                <br/>
-                @foreach($xmlLines as $key => $line)
-                    {{--*/ $number = $key + 1; /*--}}
-                    <div id="{{ $number }}"><strong style="{{ array_key_exists($number, $messages) ? 'color:red': ''  }}">{{ $number }}</strong>{{ $line }}</div>
-                @endforeach
+                <div class="xml-info">
+                    <ul>
+                        @forelse($messages as $message)
+                            <li class="error">{!! $message !!}</li>
+                        @empty
+                            <li class="success">Validated!!</li>
+                        @endforelse
+                    </ul>
+                    <br/>
+                    @foreach($xmlLines as $key => $line)
+                        {{--*/ $number = $key + 1; /*--}}
+                        <div id="{{ $number }}"
+                             style="{{ array_key_exists($number, $messages) ? 'color:#e15454;background:#eee;': ''  }}">
+                            <strong>{{ $number }}</strong>{{ $line }}</div>
+                    @endforeach
+                </div>
             </div>
         </div>
     </div>
