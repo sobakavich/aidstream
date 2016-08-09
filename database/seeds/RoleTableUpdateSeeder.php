a98ab165dbc3ddbb00a8a473c5dd2d9f922356b8
Kiran Shrestha <kiran.shrestha@yipl.com.np>
2016-08-09 14:11:59 +0545

    -[x] Json sturcture fix

--- database/seeds/RoleTableUpdateSeeder.php
+++ database/seeds/RoleTableUpdateSeeder.php
@@ -18,7 +18,7 @@ class RoleTableUpdateSeeder extends Seeder
 
         $roles = [
             ['id' => 5, 'role' => 'Administrator', 'permissions' => '["add", "edit", "delete", "publish", "settings"]', 'created_at' => '2015-09-01 11:42:29', 'updated_at' => '2015-09-01 11:43:29'],
-            ['id' => 6, 'role' => 'Editor', 'permissions' => "['add', 'edit', 'delete']", 'created_at' => '2015-09-01 11:42:29', 'updated_at' => '2015-09-01 11:43:29'],
+            ['id' => 6, 'role' => 'Editor', 'permissions' => '["add", "edit", "delete"]', 'created_at' => '2015-09-01 11:42:29', 'updated_at' => '2015-09-01 11:43:29'],
             ['id' => 7, 'role' => 'Viewer', 'permissions' => '[]', 'created_at' => '2015-09-01 11:42:29', 'updated_at' => '2015-09-01 11:43:29'],
         ];
 
