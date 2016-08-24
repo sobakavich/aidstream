b98a27f0a240df803d3c69f0f42cee10f5742e73
Sweta Shrestha <sweta.shrestha@yipl.com.np>
2016-08-24 14:28:25 +0545

    UI fixes

--- resources/views/auth/verification.blade.php
+++ resources/views/auth/verification.blade.php
@@ -3,7 +3,7 @@
         <img src={{ url('/images/ic-sent-mail.svg') }} alt="mail" width="88" height="94">
         <h1>Thank You!</h1>
         <p>
-            A Verification email has been sent to <strong>{{ session('email') }}</strong>. Click on the verify link in the email to activate your account.
+            A Verification email has been sent to <strong>{{ session('email') }}</strong>. Please click on the link in the email to verify and activate your new AidStream account.
         </p>
     </div>
 </div>
