1706e3163174dadd39d7c7642351f55f24524a48
Sachit Bir Singh Dongol <sachit.singh@yipl.com.np>
2016-08-30 10:58:34 +0545

    AS#61 Improved Activity CSV Importer  - [x] route updated  - [x] folder structure  - [x] templates added  - [x] Job dispatch error fixed  - [x] CsvProcessor added  - [x] process definition  - [x] core classes added  - [x] Simple Activity elements processor  - [x] Refactored CSV processor, other minor refactors
     Basic Fields - Data Mapping
       - [x] Title
       - [x] Identifier
       - [x] Description
       - [x] Activity Status
       - [x] Activity Date
       - [x] Participating Organisation Data Mapping
       - [x] Recipient Country Data Mapping
       - [x] Recipient Region Data Mapping
       - [x] Sector Data Mapping
       - [x] Basic CSV Template updated
       - [x] minor refactor
    
     Basic Fields - Data Validation
       - [x] upload form validated, phpDocs added
       - [x] Csv Validation Factory added
       - [x] class Foundation/IatiElement added, minor refactor
       - [x] Data validation for basic fields

---
+++ app/Services/CsvImporter/Entities/Activity/Components/ActivityRow.php
@@ -0,0 +1,105 @@
+<?php namespace App\Services\CsvImporter\Entities\Activity\Components;
+
+use App\Services\CsvImporter\Entities\Row;
+
+/**
+ * Class ActivityRow
+ * @package App\Services\CsvImporter\Entities\Activity\Components
+ */
+class ActivityRow extends Row
+{
+    /**
+     * Elements for an Activity Row.
+     * @var array
+     */
+    protected $elements = ['identifier', 'title', 'description', 'activityStatus', 'activityDate', 'participatingOrganisation', 'recipientCountry', 'recipientRegion', 'sector'];
+
+    /**
+     * @var
+     */
+    protected $identifier;
+
+    /**
+     * @var
+     */
+    protected $title;
+
+    /**
+     * @var
+     */
+    protected $description;
+
+    /**
+     * @var
+     */
+    protected $activityStatus;
+
+    /**
+     * @var
+     */
+    protected $activityDate;
+
+    protected $participatingOrganisation;
+
+    protected $recipientCountry;
+
+    protected $recipientRegion;
+
+    protected $sector;
+
+    /**
+     * Base Namespace for the Element classes.
+     */
+    const BASE_NAMESPACE = 'App\Services\CsvImporter\Entities\Activity\Components\Elements';
+
+    /**
+     * ActivityRow constructor.
+     * @param                   $fields
+     */
+    public function __construct($fields)
+    {
+        $this->fields = $fields;
+        $this->init();
+    }
+
+    /**
+     * Initialize the Row object.
+     */
+    public function init()
+    {
+        foreach ($this->elements as $element) {
+            $this->$element = $this->make($element, $this->fields(), self::BASE_NAMESPACE);
+        }
+    }
+
+    /**
+     * Process the Row.
+     * @return $this
+     */
+    public function process()
+    {
+        return $this;
+    }
+
+    /**
+     * Validate the Row.
+     * @return $this
+     */
+    public function validate()
+    {
+        foreach ($this->elements() as $element) {
+            $this->$element->validate();
+        }
+
+        return $this;
+    }
+
+    /**
+     * Store the Row in a temporary JSON File for further usage.
+     */
+    public function keep()
+    {
+        // TODO: Implement keep() method.
+        // TODO: Write validated activity into JSON.
+    }
+}
