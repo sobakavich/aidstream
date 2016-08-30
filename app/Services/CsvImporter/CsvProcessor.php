dbc97563a945bd8334631d2264f8e0806652f23e
Sachit Bir Singh Dongol <sachit.singh@yipl.com.np>
2016-08-30 17:39:47 +0545

    - [x] Job dispatch error fixed  - [x] CsvProcessor added  - [x] Simple Activity elements processor  - [x] Refactored CSV processor

--- app/Services/CsvImporter/CsvProcessor.php
+++ app/Services/CsvImporter/CsvProcessor.php
@@ -11,6 +11,13 @@ class CsvProcessor
 
     public $model;
 
+    /**
+     * @var array
+     */
+    protected $activityContainer = [];
+
+    protected $csvIdentifier = 'activity_identifier';
+
     public function __construct($csv)
     {
         $this->csv = $csv;
@@ -19,7 +26,7 @@ class CsvProcessor
     public function handle()
     {
         $this->make('App\Services\CsvImporter\Entities\Activity\Activity');
-
+        $this->groupValues($this->csv);
         $this->model->process()
                     ->validate()
                     ->keep();
@@ -32,4 +39,51 @@ class CsvProcessor
             $this->model = app()->make($class, [$this->data]);
         }
     }
+
+    /**
+     * @param $csv
+     */
+    protected function groupValues($csv)
+    {
+        $index = - 1;
+        foreach ($csv as $row) {
+            $sameElement = $this->isSameElement($row);
+            if (!$sameElement) {
+                $index ++;
+                foreach ($row as $key => $value) {
+                    $this->setValue($index, $key, $value);
+                }
+            } else {
+                foreach ($row as $key => $value) {
+                    $this->setValue($index, $key, $value);
+                }
+            }
+        }
+        dd($this->activityContainer);
+    }
+
+    protected function setValue($index, $key, $value)
+    {
+        if (!isset($this->activityContainer[$index][$key])) {
+            $this->activityContainer[$index][$key] = null;
+        }
+
+        if (!(is_null($value) || $value == "")) {
+            $this->activityContainer[$index][$key][] = $value;
+        }
+    }
+
+    /**
+     * @param $row
+     * @return bool
+     */
+    protected function isSameElement($row)
+    {
+        if (is_null($row[$this->csvIdentifier]) || $row[$this->csvIdentifier] == '') {
+            return true;
+        }
+
+        return false;
+    }
+
 }
