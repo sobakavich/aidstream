13f3821015e58d97e3482f63877c23a0c19e9028
Sachit Bir Singh Dongol <sachit.singh@yipl.com.np>
2016-09-23 13:41:43 +0545

    clear all button added for invalid activities

--- public/js/csvImporter/csvImportStatus.js
+++ public/js/csvImporter/csvImportStatus.js
@@ -2,6 +2,9 @@ var submitButton = $('input#submit-valid-activities');
 var count = 0;
 var counter = $('input#value');
 var transferComplete = false;
+var clearInvalidButton = $('#clear-invalid');
+var validParentDiv = $('.valid-data');
+var invalidParentDiv = $('.invalid-data');
 
 var CsvImportStatusManager = {
     getParentDiv: function (selector) {
@@ -26,7 +29,9 @@ var CsvImportStatusManager = {
         CsvImportStatusManager.callAsync('get-valid-data', 'GET').success(function (validData) {
             var parentDiv = CsvImportStatusManager.getParentDiv('valid-data');
 
-            parentDiv.append(validData.render);
+            if (validParentDiv.html() != 'No data available.') {
+                parentDiv.append(validData.render);
+            }
 
             CsvImportStatusManager.enableImport(validData);
         }).error(function (error) {
@@ -41,7 +46,9 @@ var CsvImportStatusManager = {
         CsvImportStatusManager.callAsync('get-invalid-data', 'GET').success(function (invalidData) {
             var parentDiv = CsvImportStatusManager.getParentDiv('invalid-data');
 
-            parentDiv.append(invalidData.render);
+            if (invalidParentDiv.html() != 'No data available.') {
+                parentDiv.append(invalidData.render);
+            }
         }).error(function (error) {
             // TODO: handle error
             var parentDiv = CsvImportStatusManager.getParentDiv('invalid-data');
@@ -72,19 +79,29 @@ var CsvImportStatusManager = {
         CsvImportStatusManager.callAsync('/import-activity/remaining-invalid-data', 'GET').success(function (response) {
             var parentDiv = CsvImportStatusManager.getParentDiv('invalid-data');
 
-            parentDiv.append(response.render);
+            if (invalidParentDiv.html() != 'No data available.') {
+                parentDiv.append(response.render);
+            }
         });
     },
     getRemainingValidData: function () {
         CsvImportStatusManager.callAsync('/import-activity/remaining-valid-data', 'GET').success(function (response) {
             var parentDiv = CsvImportStatusManager.getParentDiv('valid-data');
 
-            parentDiv.append(response.render);
+            if (validParentDiv.html() != 'No data available.') {
+                parentDiv.append(response.render);
+            }
+
         });
+    },
+    showClearButton: function () {
+        clearInvalidButton.show();
     }
 };
 
 $(document).ready(function () {
+    clearInvalidButton.hide();
+
     var interval = setInterval(function () {
         CsvImportStatusManager.isTransferComplete();
         if (CsvImportStatusManager.ifParentIsEmpty('invalid-data')) {
@@ -97,7 +114,18 @@ $(document).ready(function () {
 
         if (transferComplete) {
             test();
+            CsvImportStatusManager.showClearButton();
             clearInterval(interval);
         }
-    }, 4000);
+    }, 8000);
+
+    clearInvalidButton.on('click', function () {
+        CsvImportStatusManager.callAsync('/import-activity/clear-invalid-activities', 'GET').success(function (response) {
+            if (!CsvImportStatusManager.ifParentIsEmpty('invalid-data')) {
+                if (response == 'cleared') {
+                    CsvImportStatusManager.getCurrentData('invalid-data').empty();
+                }
+            }
+        });
+    });
 });
