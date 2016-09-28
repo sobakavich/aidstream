183b0b86e038c466d7260c93686edbae141e9df1
Sachit Bir Singh Dongol <sachit.singh@yipl.com.np>
2016-09-28 11:12:46 +0545

    - [x] tabs changed to select box

--- resources/views/Activity/csvImporter/status.blade.php
+++ resources/views/Activity/csvImporter/status.blade.php
@@ -34,12 +34,22 @@
                 <div class="col-xs-12 col-md-8 col-lg-8 element-content-wrapper element-upload-wrapper status-wrapper">
                     <div class="panel panel-default panel-upload">
                         <div class="panel-body">
-                            <ul class="nav nav-tabs" role="tablist">
-                                <li role="presentation" class="active"><a href="#valid" aria-controls="valid" role="tab" data-toggle="tab">Valid</a></li>
-                                <li role="presentation"><a href="#invalid" aria-controls="invalid" role="tab" data-toggle="tab">Invalid</a></li>
-                            </ul>
+                            <select class="tab-select">
+                                <option data-select="all">All</option>
+                                <option data-select="valid">Valid</option>
+                                <option data-select="invalid">Invalid</option>
+                            </select>
+
+                            <form action="{{ route('activity.cancel-import') }}" method="POST" id="cancel-import">
+                                {{ csrf_field() }}
+                                <input type="button" class="btn_confirm hidden" id="cancel-import" data-title="Confirmation" data-message="Are you sure you want to Cancel Activity Import?" value="Cancel">
+                            </form>
 
                             <div class="tab-content">
+                                <div role="tabpanel" class="tab-pane" id="all">
+                                    <div class="all-data"></div>
+                                </div>
+
                                 <div role="tabpanel" class="tab-pane active" id="valid">
                                     <div id="checkAll" class="hidden">
                                         <label>
@@ -52,17 +62,9 @@
                                         <div class="valid-data"></div>
                                         <input type="submit" class="hidden" id="submit-valid-activities" value="Import">
                                     </form>
-
-                                    <form action="{{ route('activity.cancel-import') }}" method="POST" id="cancel-import">
-                                        {{ csrf_field() }}
-                                        <input type="button" class="btn_confirm hidden" id="cancel-import" data-title="Confirmation" data-message="Are you sure you want to Cancel Activity Import?" value="Cancel">
-                                    </form>
                                 </div>
 
                                 <div role="tabpanel" class="tab-pane" id="invalid">
-                                    <span class="pull-right">
-                                        <a href="javascript:void(0)" id="clear-invalid">Clear all</a>
-                                    </span>
                                     <div class="invalid-data"></div>
                                 </div>
                             </div>
@@ -80,18 +82,7 @@
 @stop
 
 @section('script')
-    <script>
-        var test = function () {
-            $(".invalid-data .panel-default .panel-heading").on('click', 'label', function () {
-                $(this).children('.data-listing').slideToggle();
-            });
-        };
-
-
-        $('#check-all').on('change', function () {
-            $("input[type=checkbox]").prop('checked', $(this).prop('checked'));
-        });
-    </script>
-
+    <script src="{{ asset('js/csvImporter/accordion.js') }}"></script>
     <script src="{{ asset('js/csvImporter/csvImportStatus.js') }}"></script>
+    <script src="{{ asset('js/csvImporter/selectTabs.js') }}"></script>
 @stop
