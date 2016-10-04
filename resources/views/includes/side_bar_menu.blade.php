c8e82ea2bebd56f3fe15e25dcd779d3f177d9cc3
Kiran Shrestha <kiran.shrestha@yipl.com.np>
2016-10-04 16:56:53 +0545

    - [x] Registration Fixes - [x] User Onboarding Code Uncommented - [x] Contact administrator email template

--- resources/views/includes/side_bar_menu.blade.php
+++ resources/views/includes/side_bar_menu.blade.php
@@ -4,13 +4,10 @@
             <ul class="nav">
                 <li class="activities" id="step-1"><a href="{{ route('activity.index') }}">Activities</a></li>
                 <li class="organization" id="step-3"><a href="{{ route('organization.show', session('org_id')) }}">Organisation</a></li>
-                <li class="published-files" ><a href="{{ route('list-published-files') }}">Published Files</a></li>
-                {{--<li class="users"><a href="{{route('admin.list-users')}}">Users</a></li>--}}
+                <li class="published-files"><a href="{{ route('list-published-files') }}">Published Files</a></li>
                 <li class="documents" id="step-4"><a href="{{ route('documents') }}">Documents</a></li>
                 <li class="downloads" id="step-5"><a href="{{route('download.index')}}">Downloads</a></li>
-                @if(Auth::user()->role_id == 1)
-                    <li class="settings" id="step-6"><a href="{{ route('settings') }}">Settings</a></li>
-                @endif
+                <li class="settings" id="step-6"><a href="{{ route('settings') }}">Settings</a></li>
                 @if(Auth::user()->role_id == 1)
                     <li class="logs" id="step-7"><a href="{{ route('user-logs') }}">Activity Log</a></li>
                 @endif
