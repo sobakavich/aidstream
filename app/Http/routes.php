f41bc50103f69140cd076a7e82caea98a090977c
Rox <raj.maharjan33@gmail.com>
2016-11-09 09:58:53 +0545

    CSV generator for Data Analysis

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -455,3 +455,75 @@ $router->post('/add-publishing-info-later', [
     'as'   => 'publishing-info.add-later',
     'uses' => 'Auth\VerificationController@addPublishingInfoLater'
 ]);
+
+$router->get('csv-for-sid', function () {
+    $activityModel = app()->make(\App\Models\Activity\Activity::class);
+    $excel = app(Maatwebsite\Excel\Excel::class);
+
+    $activities = $activityModel->where('published_to_registry', 1)->with('organization', 'transactions')->get();
+
+    $activityData = [];
+
+    try {
+        foreach ($activities as $index => $activity) {
+            $data = [];
+
+            $activity_identifier = $activity->identifier['activity_identifier'];
+            $title = preg_replace('/[^A-Za-z0-9\- ]/', '', $activity->title[0]['narrative']);
+            $reporting_org_id = (getVal($activity->organization->reporting_org, [0, 'reporting_organization_identifier']));
+            $reporting_org_name = (getVal($activity->organization->reporting_org, [0, 'narrative', 0, 'narrative']));
+            $recipient_code = $activity->recipient_country ? ($activity->recipient_country[0]['country_code']) : '';
+            $recipient_country = $activity->recipient_country ? ($activity->recipient_country[0]['narrative'][0]['narrative']) : '';
+            $funding_org_identifier = ($activity->participating_organization[0]['organization_role'] == 2)?($activity->participating_organization[0]['identifier']): "";
+            $funding_org = ($activity->participating_organization[0]['organization_role'] == 2)?($activity->participating_organization[0]['narrative'][0]['narrative']): "";
+            $transaction_recipient_country_code = '';
+
+            $data[$index]['activity_identifier'] = $activity_identifier;
+            $data[$index]['title'] = $title;
+            $data[$index]['reporting_org_id'] = $reporting_org_id;
+            $data[$index]['reporting_org_name'] = $reporting_org_name;
+            $data[$index]['recipient_code'] = $recipient_code;
+            $data[$index]['recipient_country'] = $recipient_country;
+            $data[$index]['funding_org_identifier'] = $funding_org_identifier;
+            $data[$index]['funding_org'] = $funding_org;
+            $data[$index]['transaction_recipient_country_code'] = $transaction_recipient_country_code;
+
+            if (count($activity->transactions) > 1) {
+                $i = $index;
+
+                foreach ($activity->transactions as $transaction) {
+                    if (array_key_exists($i, $activity->transactions)) {
+                        $data[$i]['transaction_recipient_country_code'] = getVal($activity->transactions[$i]->transaction, ['recipient_country', 0, 'country_code'], "");
+                    }
+
+                    $i++;
+
+                        $data[$i] = [
+                            'activity_identifier' => $activity_identifier,
+                            'title' => $title,
+                            'reporting_org_id' => $reporting_org_id,
+                            'reporting_org_name' => $reporting_org_name,
+                            'recipient_code' => $recipient_code,
+                            'recipient_country' => $recipient_country,
+                            'funding_org_identifier' => $funding_org_identifier,
+                            'funding_org' => $funding_org,
+                            'transaction_recipient_country_code' => $transaction_recipient_country_code
+                        ];
+                }
+            } else {
+                $data[$index]['transaction_recipient_country_code'] = (!is_null($activity->transactions()->first())) ? getVal($activity->transactions()->first()->transaction, ['recipient_country', 0, 'country_code'], "") : '';
+            }
+            $activityData[] = $data[$index];
+
+        }
+    } catch (Exception $e) {
+
+    }
+
+    $excel->create('temp', function ($excel) use ($activityData) {
+        $excel->sheet('sheet1', function ($sheet) use ($activityData) {
+            $sheet->fromArray($activityData);
+        });
+    })->export('csv');
+
+});
